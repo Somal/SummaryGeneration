77c9bcecfc80229131db5f9659ba1e06be54760e
somal <somal1996>
2017-10-01 16:07:26 +0400

    Add logging

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -1,3 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def login():
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('client_id.json', scope)
+    gc = gspread.authorize(credentials)
+    return gc
 
 
 def create_spreadsheet(data: dict):
